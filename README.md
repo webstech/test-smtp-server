6d99c1d8f4d11997c83328f9b6f0b9efda7d8052
Chris. Webster <chris@webstech.net>
2021-11-01 21:50:51 -0700

    Initial commit
    Signed-off-by: Chris. Webster <chris@webstech.net>

---
+++ README.md
@@ -0,0 +1,72 @@
+# Test SMTP Server
+
+Test SMTP Server is a lightweight wrapper for  [smtp-server](<https://nodemailer.com/extras/smtp-server/>).
+It is primarily intended for development and testing.  The test code can start the server, run email tests
+and then validate the email contents.  It can replace the use of external fake SMTP services which may have availablility issues.
+
+All received emails are stored in an array.  The emails may be viewed as raw data or a parsed object that is easily examined.
+
+## Getting Started
+
+### Prerequisites
+
+Tested on Node v16 with npm v7.
+
+### Installation
+
+```sh
+npm install test-smtp-server --save-dev
+```
+
+### Usage
+
+See [test code](https://github.com/webstech/test-smtp-server/test/index.ts) for an example.
+
+```js
+import { testSmtpServer } from "test-smtp-server";
+
+(async (): Promise<void> => {
+  const smtpserver = new testSmtpServer();
+  await smtpserver.startServer();   // start listening
+
+  // send some emails capturing ids ..
+  messageId.unshift( await sendMail(email, smtpOptions));
+
+  const mails = smtpserver.getEmails();
+
+  // validate/dump emails
+
+  if (mails.length) {
+    let entry = 0;
+    for (const mail of mails) {
+      console.log(`Checking mail entry <${entry}>`);
+      console.log(mail.envelope);
+      const parsed = await mail.getParsed();
+      if (parsed.messageId !== messageId[entry]) {
+        throw new Error(`Messageids do not match for email ${
+          entry} <${parsed.messageId}> <${messageId[entry]}>`);
+      }
+
+      console.log(parsed);
+      entry++;
+    }
+  } else {
+    throw new Error("No emails captured when expected");
+  }
+
+  await smtpserver.stopServer();    // terminate server
+```
+
+### Security
+
+The server is started as secure but that may not be possibly in the tesing
+environment.  If the server does not have a valid certificate, the connection
+will fail from the client side. Some clients (e.g. nodemailer/SMTP-Transport options)
+allow connections to non-secure servers.  Node allows connections through an
+environment variable that turns off certificate authorization chaecking.  See
+[node_tls_reject_unauthorizedvalue](https://nodejs.org/api/cli.html#node_tls_reject_unauthorizedvalue)
+for more information.  This may be insecure if other ports are used.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ lib/test-smtp-server.ts
@@ -0,0 +1,162 @@
+import { SMTPServer, SMTPServerEnvelope } from "smtp-server";
+import { simpleParser, ParsedMail } from "mailparser";
+import { Writable, Readable } from "stream";
+
+/**
+ * Class to describe an email.  The `envelope` contains the SMTP routing
+ * information.  This may not match the `to/cc/bcc` information in the
+ * email contaents.  The buffer contains the raw email content.  To easily
+ * access parts of the email, `getParsed` should be used.
+ */
+export class eMail {
+  envelope: SMTPServerEnvelope;
+  buffer: Buffer | null = null;
+  length = 0;
+  constructor(envelope: SMTPServerEnvelope) {
+    this.envelope = envelope;
+  }
+
+  /**
+   * Return a parsed email as formatted by `simpleParser`.
+   *
+   * @returns Promise<ParsedMail>  See
+   * https://nodemailer.com/extras/mailparser/ for the structure.
+   */
+  async getParsed(): Promise<ParsedMail> {
+    if (this.buffer) {
+      const stream = Readable.from(this.buffer);
+
+      return simpleParser(stream);
+      /*
+      return simpleParser(stream).then((email) => {
+        return email;
+      });
+      */
+    } else {
+      throw new Error("Empty email buffer");
+    }
+  }
+}
+
+/**
+ * testSmtpServer optional parameters
+ */
+export type testSmtpServerOptions = {
+  // smtpHost?: string;
+
+  /** the port number to use (default: 1025) */
+  smtpPort?: number;
+
+  /** logging function like console.log() */
+  debug?: (message?: unknown, ...optionalParams: any[]) => void;
+}
+
+/**
+ * Create a testSmtpServer.  This provides a wrapper to SMTPServer that
+ * can be used for testing.  The emails are stored in an array that
+ * can be examined to validate the content.
+ */
+export class testSmtpServer {
+  private port: number;
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private debug = (_message?: unknown, ..._optionalParams: any[]) => {};
+  // private host: string;
+  private emails: eMail[] = [];
+  private server: SMTPServer;
+
+  public constructor(options?: testSmtpServerOptions | undefined) {
+    this.port = 1025;
+    if (options) {
+      if (options.smtpPort) {
+        this.port = options.smtpPort;
+      }
+
+      if (options.debug) {
+        this.debug = options.debug;
+      }
+    }
+    // this.host =  "localhost";
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const that = this;              // preserve for use in other objects
+    this.server = new SMTPServer({
+      authOptional: true,
+      onConnect(session, callback) {
+        if (session.remoteAddress !== "127.0.0.1") {
+          return callback(new Error("Only connections from localhost allowed"));
+        }
+
+        return callback();          // Accept the connection
+      },
+
+      onAuth(auth, _session, callback) {
+        that.debug(`SMTP login for user: ${auth.username}`);
+        callback(null, { user: auth.username });
+      },
+
+      onData(stream, session, callback) {
+        const buffers: Buffer[] = [];
+        const writer = new Writable({
+          write(data: Buffer, _encoding, writerCallback) {
+            buffers.push(data);
+            writerCallback();
+          },
+        });
+        stream.pipe(writer);
+        stream.on("end", () => {
+          const email = new eMail(session.envelope);
+          email.buffer = Buffer.concat(buffers);
+          // buf.pipe(process.stdout);
+          that.emails.unshift(email);
+          that.debug(JSON.stringify(email, undefined, 2));
+          callback();
+        });
+      },
+      secure: true,
+    });
+
+    /*
+    this.server.on("error", (err) => {
+      console.log(err);
+    });
+    */
+  }
+
+  /**
+   * Clear the set of emails.
+   *
+   * @returns number of emails deleted
+   */
+   public clearEmails(): number {
+    const count = this.emails.length;
+    this.emails.length = 0;
+    return count;
+  }
+
+  /**
+   * Retrieve the set of emails in order from latest to oldest.
+   *
+   * @returns array of eMail objects
+   */
+   public getEmails(): eMail[] {
+    return this.emails;
+  }
+
+  /**
+   * Query for the port number used by the server.
+   *
+   * @returns the port number being used
+   */
+  public getPort(): number {
+    return this.port;
+  }
+
+  /** Start the server */
+  public startServer(): void {
+    this.server.listen(this.port);
+  }
+
+  /** Stop the server */
+  public stopServer(): void {
+    this.server.close();
+  }
+}
